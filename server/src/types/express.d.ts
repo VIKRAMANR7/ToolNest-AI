07ee7b4bbc883d563fc986020439a644a0b62c39
Vikraman <vkram777@gmail.com>
2025-11-27 08:25:08 +0530

    small type fixes

--- server/src/types/express.d.ts
+++ server/src/types/express.d.ts
@@ -1,16 +1,13 @@
 import "express";
-
 declare global {
   namespace Express {
     interface Request {
       userId?: string;
       plan?: "free" | "premium";
       free_usage?: number;
-      auth?: () => Promise<{
-        userId?: string;
-        has?: (input: { plan: string }) => Promise<boolean>;
-      }>;
       file?: Express.Multer.File;
     }
   }
 }
+
+export {};
